4a5b92ff75913b762c9315a541b48f738b2b5c8d
Furior <furiorg@gmail.com>
2024-05-24 21:27:25 +0700

    ref: damage redirecting
    typo

--- quant.ew/files/cbs/redirect_damage_to_host.lua
+++ quant.ew/files/cbs/redirect_damage_to_host.lua
@@ -1,26 +1,13 @@
-function inflict_direct_damage(entity_id, damage)
-    local damageModelComponent = EntityGetFirstComponentIncludingDisabled(entity_id, "DamageModelComponent")
-    if not damageModelComponent then
-        return
-    end
-    local health = ComponentGetValue2(damageModelComponent, "hp")
-    if not health then
-        return
-    end
-    new_health = health - damage
-    ComponentSetValue2(damageModelComponent, "hp", new_health)
-    if new_health <= 0 then
-        EntityKill(entity_id)
-    end
-    
-end
-
 -- Called on host when clients get damage and redirects it straight to the host's hp, ignoring any resists.
 function damage_received(damage, message, entity_thats_responsible, is_fatal, projectile_thats_responsible)
     -- Damage the host
     local host_entity_id = EntityGetWithTag("ew_host")[1]
-    inflict_direct_damage(host_entity_id, damage)
-    -- Return the health to the client
-    local client_entity_id = GetUpdatedEntityID(client_entity_id, -damage)
-    inflict_direct_damage(client_entity_id, -damage)
+	local protection_component_id = GameGetGameEffect(host_entity_id, "PROTECTION_ALL")
+	if protection_component_id then
+        EntitySetComponentIsEnabled(host_entity_id, protection_component_id, false)
+	end
+    EntityInflictDamage(host_entity_id, damage, "DAMAGE_CURSE", message, "NONE", 0, 0, entity_thats_responsible)
+    if protection_component_id then
+        EntitySetComponentIsEnabled(host_entity_id, protection_component_id, true)
+    end
 end
