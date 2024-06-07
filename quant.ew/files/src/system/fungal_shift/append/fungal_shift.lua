f6c98f06827934c7429b0098ca2d38e2bfdadf33
IQuant <quant3234@gmail.com>
2024-06-07 21:34:43 +0300

    Fix "empty" fungal shifts, fix material message not being present.

--- quant.ew/files/src/system/fungal_shift/append/fungal_shift.lua
+++ quant.ew/files/src/system/fungal_shift/append/fungal_shift.lua
@@ -4,11 +4,13 @@ function fungal_shift(entity, x, y, debug_no_limits)
     local old_convert_mat = ConvertMaterialEverywhere
     local old_get_text = GameTextGet
     local from_material_name = ""
+    local changed_anything = false
     function ConvertMaterialEverywhere(mat_from, mat_to)
         old_convert_mat(mat_from, mat_to)
+        changed_anything = true
         CrossCall("ew_fungal_shift_conversion", mat_from, mat_to)
     end
-    function GameGetText(arg1, arg2)
+    function GameTextGet(arg1, arg2)
         if arg1 == "$logdesc_reality_mutation" then
             from_material_name = arg2
         end
@@ -17,5 +19,7 @@ function fungal_shift(entity, x, y, debug_no_limits)
     ConvertMaterialEverywhere = old_convert_mat
     GameTextGet = old_get_text
     local iter = tonumber(GlobalsGetValue( "fungal_shift_iteration", "0" ))
-    CrossCall("ew_fungal_shift", iter, from_material_name)
+    if changed_anything then
+        CrossCall("ew_fungal_shift", iter, from_material_name)
+    end
 end
