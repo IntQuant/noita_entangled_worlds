a4700dd313b632f7983b2fffb7c543c77d0b7679
IQuant <quant3234@gmail.com>
2024-06-30 18:57:07 +0300

    You take 5 times as much damage when you have glass cannon.

--- quant.ew/files/src/system/damage/cbs/adjust_damage.lua
+++ quant.ew/files/src/system/damage/cbs/adjust_damage.lua
@@ -5,6 +5,11 @@ function adjust_damage.adjust_damage(dmg, details)
     if details == 16 or details == 32768 or details == 65536 then
         local player_count = tonumber(GlobalsGetValue("ew_player_count", "1"))
         return dmg / player_count
+    else
+        local has_glass_cannon = GlobalsGetValue("PERK_PICKED_GLASS_CANNON_PICKUP_COUNT", "0") ~= "0"
+        if has_glass_cannon then
+            return dmg * 5
+        end
     end
     
     return dmg
--- quant.ew/files/src/system/perk_patches/append/perk_list.lua
+++ quant.ew/files/src/system/perk_patches/append/perk_list.lua
@@ -1,4 +1,4 @@
-local function patch_perk(perk_id, fn, ignore_original_func)
+local function patch_perk(perk_id, ignore_original_func, fn)
     local perk_data = get_perk_with_id(perk_list, perk_id)
     local old_func = perk_data.func
     perk_data.func = function(entity_perk_item, entity_who_picked, item_name, pickup_count)
@@ -14,17 +14,21 @@ local function hide_perk(perk_id)
     perk_data.not_in_default_perk_pool = true
 end
 
-patch_perk("EXTRA_HP", function(entity_perk_item, entity_who_picked)
+patch_perk("EXTRA_HP", false, function(entity_perk_item, entity_who_picked)
     if EntityHasTag(entity_who_picked, "player_unit") then
         CrossCall("ew_perks_modify_max_hp", 1.5, true)
     end
 end)
 
-patch_perk("VAMPIRISM", function(entity_perk_item, entity_who_picked)
+patch_perk("VAMPIRISM", true, function(entity_perk_item, entity_who_picked)
     if EntityHasTag(entity_who_picked, "player_unit") then
         CrossCall("ew_perks_modify_max_hp", 0.75)
     end
-end, true)
+end)
+
+patch_perk("GLASS_CANNON", true, function()
+    -- Intentionally empty.
+end)
 
 hide_perk("ABILITY_ACTIONS_MATERIALIZED")
 hide_perk("RESPAWN")
