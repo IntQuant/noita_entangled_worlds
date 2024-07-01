7f86a69a10064327780aa537bb9bc1916fe47f3d
IQuant <quant3234@gmail.com>
2024-07-01 21:23:16 +0300

    Added icon and metadata to windows exe.

--- noita-proxy/build.rs
+++ noita-proxy/build.rs
@@ -1,4 +1,11 @@
 fn main() {
     #[cfg(target_os = "linux")]
     println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
+
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("assets/icon.ico");
+
+        res.compile().unwrap();
+    }
 }
