2449ebe22f86dce81de5923744e9ac6f9dc70a73
IQuant <quant3234@gmail.com>
2024-08-13 15:30:08 +0300

    Add more entries to phys sync whitelist.

--- quant.ew/files/core/constants.lua
+++ quant.ew/files/core/constants.lua
@@ -4,7 +4,44 @@ module.phys_sync_allowed = {
     ["data/entities/props/physics_propane_tank.xml"] = true,
     ["data/entities/props/physics_box_explosive.xml"] = true,
     ["data/entities/props/physics_crate.xml.xml"] = true,
+    
+    ["data/entities/props/physics_barrel_oil.xml"] = true,
     ["data/entities/props/physics_barrel_radioactive.xml"] = true,
+    ["data/entities/props/physics_seamine.xml"] = true,
+    ["data/entities/props/suspended_tank_radioactive.xml"] = true,
+    ["data/entities/props/suspended_tank_acid.xml"] = true,
+    
+    ["data/entities/props/physics_box_harmless.xml"] = true,
+    ["data/entities/props/physics_tubelamp.xml"] = true,
+
+    ["data/entities/props/physics_torch_stand.xml"] = true,
+    ["data/entities/props/vault_apparatus_01.xml"] = true,
+    ["data/entities/props/vault_apparatus_02.xml"] = true,
+    ["data/entities/props/physics_pressure_tank.xml"] = true,
+    
+    ["data/entities/props/crystal_red.xml"] = true,
+    
+    -- HM statues
+    ["data/entities/props/temple_statue_01.xml"] = true,
+    ["data/entities/props/temple_statue_02.xml"] = true,
+
+    -- Traps
+    ["entities/props/physics_trap_circle_acid.xml"] = true,
+    ["entities/props/physics_trap_electricity_enabled.xml"] = true,
+    ["entities/props/physics_trap_electricity.xml"] = true,
+    ["entities/props/physics_trap_ignite_enabled.xml"] = true,
+    ["entities/props/physics_trap_ignite.xml"] = true,
+    ["entities/props/physics/trap_circle_acid.xml"] = true,
+    ["entities/props/physics/trap_electricity_enabled.xml"] = true,
+    ["entities/props/physics/trap_electricity_suspended.xml"] = true,
+    ["entities/props/physics/trap_electricity.xml"] = true,
+    ["entities/props/physics/trap_ignite_enabled.xml"] = true,
+    ["entities/props/physics/trap_ignite.xml"] = true,
+    ["entities/props/physics/trap_laser_enabled_left.xml"] = true,
+    ["entities/props/physics/trap_laser_enabled.xml"] = true,
+    ["entities/props/physics/trap_laser_toggling_left.xml"] = true,
+    ["entities/props/physics/trap_laser_toggling.xml"] = true,
+    ["entities/props/physics/trap_laser.xml"] = true,
 }
 
 return module
