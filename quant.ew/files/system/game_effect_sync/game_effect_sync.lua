98766d8695b85ba23a72a0c5eda7bf1b61968c3e
IQuant <quant3234@gmail.com>
2024-08-20 00:09:06 +0300

    Yeet imports in game_effects_sync

--- quant.ew/files/system/game_effect_sync/game_effect_sync.lua
+++ quant.ew/files/system/game_effect_sync/game_effect_sync.lua
@@ -1,10 +1,5 @@
 -- This module syncs effects (like being on fire) from clients to everyone else.
 
-local ctx = dofile_once("mods/quant.ew/files/src/ctx.lua")
-local net = dofile_once("mods/quant.ew/files/src/net.lua")
-local player_fns = dofile_once("mods/quant.ew/files/src/player_fns.lua")
-local np = require("noitapatcher")
-
 local rpc = net.new_rpc_namespace()
 
 local effect_sync = {}
