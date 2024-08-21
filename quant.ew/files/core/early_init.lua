766cfb6b9144a37fc49de625d58ecf65829821a0
IQuant <quant3234@gmail.com>
2024-08-21 23:19:29 +0300

    Keep unsynced flags in some files.

--- quant.ew/files/core/early_init.lua
+++ quant.ew/files/core/early_init.lua
@@ -5,29 +5,33 @@ ModLuaFileAppend("data/scripts/init.lua", "mods/quant.ew/files/resource/append/n
 local uses_persistent_flags = {
     "data/scripts/debug/persistent_flags_check.lua",
     "data/scripts/init.lua",
-    "data/scripts/animals/boss_dragon_death.lua",
     "data/scripts/items/utility_box.lua",
     "data/scripts/items/generate_shop_item.lua",
     "data/scripts/items/make_random_card.lua",
     "data/scripts/gun/gun_actions.lua",
     "data/scripts/items/chest_random.lua",
-    "data/scripts/buildings/huussi.lua",
-    "data/scripts/perks/perk.lua",
     "data/scripts/buildings/teleroom.lua",
-    "data/scripts/buildings/chest_light.lua",
-    "data/scripts/buildings/chest_dark.lua",
     "data/scripts/item_spawnlists.lua",
     "data/scripts/magic/moon_altar_init.lua",
     "data/scripts/magic/dark_moon_altar.lua",
     "data/scripts/magic/dark_moon_altar_init.lua",
-    "data/scripts/magic/amulet.lua",
     "data/scripts/magic/moon_altar.lua",
     "data/scripts/biome_modifiers.lua",
     "data/scripts/biomes/scale.lua",
-    "data/scripts/biomes/mountain_tree.lua",
-    "data/entities/animals/boss_alchemist/death.lua",
-    "data/entities/animals/boss_pit/boss_pit_death.lua",
-    "data/entities/animals/boss_limbs/boss_limbs_death.lua",
+    
+    -- The tree shows your progress, should be fine to not sync it
+    -- "data/scripts/biomes/mountain_tree.lua",
+    
+    -- The following also modify flags.
+    -- "data/scripts/animals/boss_dragon_death.lua",
+    -- "data/scripts/buildings/huussi.lua",
+    -- "data/scripts/perks/perk.lua",
+    -- "data/scripts/buildings/chest_light.lua",
+    -- "data/scripts/buildings/chest_dark.lua",
+    -- "data/scripts/magic/amulet.lua",
+    -- "data/entities/animals/boss_alchemist/death.lua",
+    -- "data/entities/animals/boss_pit/boss_pit_death.lua",
+    -- "data/entities/animals/boss_limbs/boss_limbs_death.lua",
 }
 
 -- Override HasFlagPersistent in each.
---
+++ quant.ew/files/system/flag_sync/readme.md
@@ -0,0 +1,13 @@
+These files both read and write flags:
+
+entities/animals/boss_alchemist/death.lua           |   checks if should spawn hp refill
+entities/animals/boss_limbs/boss_limbs_death.lua    |   checks if should spawn hp refill
+entities/animals/boss_pit/boss_pit_death.lua        |   checks if should spawn hp refill
+scripts/animals/boss_dragon_death.lua               |   affects first drop
+scripts/buildings/chest_dark.lua                    |   no unlock :(
+scripts/buildings/chest_light.lua                   |   no unlock :(
+scripts/buildings/huussi.lua                        |   no unlock :(
+scripts/magic/amulet.lua                            |   gives extra unlocks
+scripts/magic/dark_moon_altar.lua                   |   idk, but seems to run in some specific case so probably fine
+scripts/magic/moon_altar.lua                        |   see scripts/magic/dark_moon_altar.lua
+scripts/perks/perk.lua                              |   tracks perk progress
