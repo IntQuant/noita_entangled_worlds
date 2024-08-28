92f4a4f3878a9f996285f8b36b469384d4f678da
bgkillas <walhelm@proton.me>
2024-08-28 19:17:43 -0400

    use less data for game effects sync

--- quant.ew/files/system/game_effect_sync/game_effect_sync.lua
+++ quant.ew/files/system/game_effect_sync/game_effect_sync.lua
@@ -26,16 +26,39 @@ function effect_sync.get_ent_effects(entity)
     return list
 end
 
+local function name_to_num(name)
+    for i, file_name in ipairs(constants.game_effects) do
+        if file_name == name then
+            return i
+        end
+    end
+    return -1
+end
+
 local local_by_remote_id = {}
 
 function effect_sync.on_world_update()
     if GameGetFrameNum() % 30 == 9 then
         local effects = effect_sync.get_ent_effects(ctx.my_player.entity)
         local sync_data = {}
+        local by_filenames = false
         for _, effect in ipairs(effects) do
-            table.insert(sync_data, {effect, EntityGetFilename(effect)})
+            local num = name_to_num(EntityGetFilename(effect))
+            if num ~= -1 then
+                table.insert(sync_data, {effect, num})
+            else
+                by_filenames = true
+                break
+            end
         end
-        rpc.send_effects(sync_data)
+        if by_filenames then
+            for _, effect in ipairs(effects) do
+                table.insert(sync_data, {effect, EntityGetFilename(effect)})
+            end
+            rpc.send_effects(sync_data, true)
+        else
+            rpc.send_effects(sync_data, false)
+        end
     end
     -- Cleanup
     if GameGetFrameNum() % 120 == 9 then
@@ -54,7 +77,7 @@ function effect_sync.remove_all_effects(entity)
     end
 end
 
-function rpc.send_effects(effects)
+function rpc.send_effects(effects, by_filenames)
     local entity = ctx.rpc_player_data.entity
     if not EntityGetIsAlive(entity) then
         return
@@ -67,14 +90,25 @@ function rpc.send_effects(effects)
             for _, old_effect in ipairs(old_local_effects) do
                 local old_com = EntityGetFirstComponentIncludingDisabled(old_effect, "GameEffectComponent")
                 if old_com ~= nil then
-                    if effect[2] == EntityGetFilename(old_effect) then
+                    local is_same
+                    if by_filenames then
+                        is_same = effect[2] == EntityGetFilename(old_effect)
+                    else
+                        is_same = constants.game_effects[effect[2]] == EntityGetFilename(old_effect)
+                    end
+                    if is_same then
                         ComponentSetValue2(old_com, "frames", 999999999)
                         local_by_remote_id[effect_remote_id] = old_effect
                         goto continue
                     end
                 end
             end
-            local ent = EntityLoad(effect[2])
+            local ent
+            if by_filenames then
+                ent = EntityLoad(effect[2])
+            else
+                ent = EntityLoad(constants.game_effects[effect[2]])
+            end
             EntityAddChild(entity, ent)
             local_by_remote_id[effect_remote_id] = ent
             local com = EntityGetFirstComponentIncludingDisabled(ent, "GameEffectComponent")
