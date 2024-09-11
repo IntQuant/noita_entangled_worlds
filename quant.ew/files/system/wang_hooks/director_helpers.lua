fbdbb30d60b086fcb191f68eaabeee9454ba9ef0
Nikita Tomashevich <quant3234@gmail.com>
2024-09-11 13:46:21 +0300

    Try to add a detour to whatever is called by RegisterSpawnFunction

--- quant.ew/files/system/wang_hooks/director_helpers.lua
+++ quant.ew/files/system/wang_hooks/director_helpers.lua
@@ -1,5 +1,23 @@
 local orig_RegisterSpawnFunction = RegisterSpawnFunction
 
+local orig_do_mod_appends = do_mod_appends
+
+-- The root file that imported everything else.
+-- TODO: maybe dofile should be patched?
+EwImportRoot = "???"
+
+orig_do_mod_appends = do_mod_appends
+do_mod_appends = function(filename, ...)
+    --do_mod_appends = orig_do_mod_appends
+    orig_do_mod_appends(filename, ...)
+    EwImportRoot = filename
+end
+
 function RegisterSpawnFunction(color, fn_name)
-        
+    local root_id = "???" -- TODO, this should be id (path) of a file that actually calls RegisterSpawnFunction, that we can dofile to call related functions manually.
+    detour_fn_name = "ew_detour_"..fn_name
+    _G[detour_fn_name] = function(...)
+        _G[fn_name](...)
+    end
+    RegisterSpawnFunction(color, detour_fn_name)
 end
--- quant.ew/files/system/wang_hooks/wang_hooks.lua
+++ quant.ew/files/system/wang_hooks/wang_hooks.lua
@@ -2,21 +2,4 @@ ModLuaFileAppend("data/scripts/director_helpers.lua", "mods/quant.ew/files/syste
 
 local module = {}
 
-local module.files_with_spawnhooks = {}
-
-for line in string.gmatch(ModTextFileGetContent("mods/quant.ew/files/system/wang_hooks/files_with_spawnhooks.txt"), "(.-)\n") do
-    -- print("Interned", line)
-    table.insert(module.files_with_spawnhooks, line)
-end
-
-local function patch_file(filename)
-
-end
-
-function module.on_late_init()
-    for _, filename in ipairs(module.files_with_spawnhooks) do
-        patch_file(filename)
-    end
-end
-
 return module
