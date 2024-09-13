640470654b60393e50fdae447dd490f42a90b225
IQuant <quant3234@gmail.com>
2024-09-13 17:42:00 +0300

    STILL doesn't work.

--- quant.ew/files/system/wang_hooks/wang_hooks.lua
+++ quant.ew/files/system/wang_hooks/wang_hooks.lua
@@ -9,10 +9,32 @@ for line in string.gmatch(ModTextFileGetContent("mods/quant.ew/files/system/wang
     table.insert(module.files_with_spawnhooks, line)
 end
 
+local current_file = nil
+
+local function patch_fn(color, orig_fn_name)
+    -- print(color, orig_fn_name)
+    local current_file = current_file
+
+    detour_fn_name = "ew_detour_" .. orig_fn_name
+
+    detour_fn = "function " .. detour_fn_name .. [[(...)
+        print("Called", "]] .. current_file .. [[", "]] .. detour_fn_name .. [[")
+        ]] .. orig_fn_name .. [[(...)
+    end
+    ]]
+    
+    new_fn_call = "RegisterSpawnFunction( " .. color .. ', "' .. detour_fn_name .. '" )'
+    repl = new_fn_call .. "\n" .. detour_fn
+    -- print(repl)
+    return repl
+end
+
 local function patch_file(filename)
     print("Patching", filename)
     local content = ModTextFileGetContent(filename)
-    content = string.gsub(content)
+    current_file = filename
+    -- A textbook example of how to NOT use regular expressions.
+    content = string.gsub(content, 'RegisterSpawnFunction[(][ ]?(.-), "(.-)"[ ]?[)]', patch_fn)
     ModTextFileSetContent(filename, content)
 end
 
