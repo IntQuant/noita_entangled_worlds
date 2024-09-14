7677e589e049bd26d5373ca5814a282c4e1e55fa
bgkillas <walhelm@proton.me>
2024-09-14 05:55:52 -0400

    disable hax preload again

--- quant.ew/init.lua
+++ quant.ew/init.lua
@@ -93,7 +93,7 @@ local function load_modules()
     ctx.load_system("essence_sync")
     ctx.load_system("spectate")
     ctx.load_system("effect_data_sync")
-    ctx.load_system("hax_preload")
+    -- ctx.load_system("hax_preload")
     -- ctx.load_system("wang_hooks")
 end
 
