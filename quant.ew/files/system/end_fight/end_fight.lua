418ace202d468af3d8c7aaf6597ea4ad4d55d695
bgkillas <walhelm@proton.me>
2024-09-21 08:23:16 -0400

    fix end fight scene, fix notplayer not shooting after throwing stuff again

--- quant.ew/files/system/end_fight/end_fight.lua
+++ quant.ew/files/system/end_fight/end_fight.lua
@@ -2,45 +2,50 @@ local end_fight = {}
 local first = true
 local try_kill = -1
 local wait_to_heal = false
+local init = -1
 
-ModLuaFileAppend("data/entities/animals/boss_centipede/ending/sampo_start_ending_sequence.lua", "mods/quant.ew/files/system/end_fight/ending_sequence_append.lua")
+ModTextFileSetContent("data/entities/animals/boss_centipede/ending/gold_effect.xml", "<Entity/>")
 
 function end_fight.on_world_update()
     if GameHasFlagRun("ending_game_completed") then
-        local exists = false
-        for peer_id, playerdata in pairs(ctx.players) do
-            if peer_id ~= ctx.my_id and not EntityHasTag(playerdata.entity, "ew_notplayer") then
-                exists = true
-                GenomeSetHerdId(playerdata.entity, "player_pvp")
-            end
-        end
-        if wait_to_heal and not EntityHasTag(ctx.my_player.entity, "ew_notplayer") then
-            async(function()
-                wait(1)
-                EntityInflictDamage(ctx.my_player.entity, 100000000, "DAMAGE_HEALING", "", "None", 0, 0, GameGetWorldStateEntity())
-            end)
-            wait_to_heal = false
-        end
-        if first then
+        if init == -1 then
             if EntityHasTag(ctx.my_player.entity, "ew_notplayer") then
+                EntityInflictDamage(ctx.my_player.entity, 100000000, "DAMAGE_CURSE", "", "None", 0, 0, GameGetWorldStateEntity())
                 wait_to_heal = true
             else
                 async(function()
-                    wait(1)
-                    EntityInflictDamage(ctx.my_player.entity, 100000000, "DAMAGE_HEALING", "", "None", 0, 0, GameGetWorldStateEntity())
+                    wait(3)
+                    EntityInflictDamage(ctx.my_player.entity, -100000000, "DAMAGE_HEALING", "", "None", 0, 0, GameGetWorldStateEntity())
                 end)
             end
             GamePrintImportant("Fight for the spoils")
             first = false
-        elseif not exists then
-            if try_kill == GameGetFrameNum() then
-                local x, y = EntityGetTransform(ctx.my_player.entity)
-                EntityLoad("data/entities/animals/boss_centipede/ending/gold_effect.xml", x, y )
-            elseif try_kill == -1 then
-                try_kill = GameGetFrameNum() + 180
+            init = GameGetFrameNum() + 10
+        elseif init < GameGetFrameNum() then
+            local exists = false
+            for peer_id, playerdata in pairs(ctx.players) do
+                if peer_id ~= ctx.my_id and not EntityHasTag(playerdata.entity, "ew_notplayer") then
+                    exists = true
+                    GenomeSetHerdId(playerdata.entity, "player_pvp")
+                end
             end
-        else
-            try_kill = -1
+            if wait_to_heal and not EntityHasTag(ctx.my_player.entity, "ew_notplayer") then
+                async(function()
+                    wait(3)
+                    EntityInflictDamage(ctx.my_player.entity, -100000000, "DAMAGE_HEALING", "", "None", 0, 0, GameGetWorldStateEntity())
+                end)
+                wait_to_heal = false
+            end
+            if not exists and not EntityHasTag(ctx.my_player.entity, "ew_notplayer") then
+                if try_kill == GameGetFrameNum() then
+                    local x, y = EntityGetTransform(ctx.my_player.entity)
+                    EntityLoad("mods/quant.ew/files/system/end_fight/gold_effect.xml", x, y )
+                elseif try_kill == -1 then
+                    try_kill = GameGetFrameNum() + 180
+                end
+            else
+                try_kill = -1
+            end
         end
     end
 end
