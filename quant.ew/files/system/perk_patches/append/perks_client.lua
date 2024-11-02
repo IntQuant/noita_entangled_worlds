0bdb95c3ffa36b142abedffccefce8b8f5ece537
bgkillas <walhelm@proton.me>
2024-11-02 19:06:27 -0400

    fix perks without random option being random

--- quant.ew/files/system/perk_patches/append/perks_client.lua
+++ quant.ew/files/system/perk_patches/append/perks_client.lua
@@ -1,3 +1,7 @@
+if not ctx.proxy_opts.randomize_perks then
+    return
+end
+
 local function hide_perk(perk_id)
     local perk_data = get_perk_with_id(perk_list, perk_id)
     perk_data.not_in_default_perk_pool = true
