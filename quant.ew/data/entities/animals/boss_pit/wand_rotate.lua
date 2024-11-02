cedc33454754236717680658d525ef5f47f91bb6
bgkillas <walhelm@proton.me>
2024-11-02 18:46:56 -0400

    make pit boss aim at closest alive player

---
+++ quant.ew/data/entities/animals/boss_pit/wand_rotate.lua
@@ -0,0 +1,19 @@
+dofile_once("data/scripts/lib/utilities.lua")
+
+local entity_id = GetUpdatedEntityID()
+local x, y = EntityGetTransform(GetUpdatedEntityID())
+
+local players = EntityGetWithTag("ew_peer")
+
+local closest
+local dir = 0
+for _, player in ipairs(players) do
+    local px, py = EntityGetTransform(player)
+    local r = px * px + py * py
+    if closest == nil or r < closest then
+        closest = r
+        dir = math.atan2(py - y, px - x)
+    end
+end
+
+EntitySetTransform(entity_id, x, y, dir)
