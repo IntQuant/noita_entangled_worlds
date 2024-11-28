87001918664c1c2501e2e7d6e29f80afa227e826
IQuant <quant3234@gmail.com>
2024-11-28 20:46:46 +0300

    Update ci script

--- scripts/check_pre_ci.py
+++ scripts/check_pre_ci.py
@@ -30,7 +30,7 @@ It should be detected automatically as long as you use steam version of the game
     notes.p("There is a button in bottom-left corner on noita-proxy's main screen that allows to auto-update to a new version when one is available")
 
     print()
-    notes_path = "/tmp/rnotes.md"
+    notes_path = "./last_release_notes.md"
     with open(notes_path, "w") as f:
         print(notes.gen_md(), file=f)
 
@@ -45,9 +45,11 @@ def main():
         exit(1)
 
     subprocess.run(["git", "pull"])
-    subprocess.run(["git", "commit", "-am", "Automated commit: "+tag])
-
+    
     notes_path = generate_notes(tag)
+
+    subprocess.run(["git", "add", "-A"])
+    subprocess.run(["git", "commit", "-am", "Automated commit: "+tag])
     
     subprocess.check_call(["git", "tag", "-a", "-F", notes_path, tag ])
     subprocess.check_call(["git", "push", "--follow-tags"])
