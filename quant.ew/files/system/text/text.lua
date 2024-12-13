d246950e5df9c1ffce438252e6030fae8fc2d731
bgkillas <walhelm@proton.me>
2024-12-13 10:38:11 -0500

    fix chat

--- quant.ew/files/system/text/text.lua
+++ quant.ew/files/system/text/text.lua
@@ -225,9 +225,8 @@ function create_chat_hint(hint) --maybe will make it for all hints in future if 
 end
 
 function module.on_world_update()
+    GuiStartFrame(gui)
     if not ModSettingGet("quant.ew.notext") and not ModSettingGet("quant.ew.nochathint") then
-        GuiStartFrame(gui)
-
         if unread_messages_counter > 0 then --prevents hint from appearing all the time (can be annoying) and just appear when there is some unread message
             create_chat_hint("Use 'Enter' to open chat.(" .. unread_messages_counter .. " unread messages)")
         end
