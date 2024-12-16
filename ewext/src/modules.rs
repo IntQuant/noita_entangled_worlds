69557d6f8dc92eb15cc457cc23aa11afc8bd2b78
IQuant <quant3234@gmail.com>
2024-12-16 16:52:08 +0300

    ewext: keep self loaded in memory between new games.

--- ewext/src/modules.rs
+++ ewext/src/modules.rs
@@ -2,9 +2,11 @@ use eyre::Ok;
 
 pub(crate) mod entity_sync;
 
+pub(crate) struct ModuleCtx {}
+
 pub(crate) trait Module {
     // fn init() -> Self;
-    fn on_world_update(&mut self) -> eyre::Result<()> {
+    fn on_world_update(&mut self, _ctx: &mut ModuleCtx) -> eyre::Result<()> {
         Ok(())
     }
 }
