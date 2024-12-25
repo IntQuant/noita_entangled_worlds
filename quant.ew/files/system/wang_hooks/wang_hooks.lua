c8050bc452e19bdb34dd24fc2fa92a476029e0a4
IQuant <quant3234@gmail.com>
2024-12-25 18:39:37 +0300

    Also sync dragonspot spawn

--- quant.ew/files/system/wang_hooks/wang_hooks.lua
+++ quant.ew/files/system/wang_hooks/wang_hooks.lua
@@ -43,6 +43,7 @@ local allow_patching = {
     spawn_shopitem = true,
     spawn_statue = true,
     spawn_crate = true,
+    spawn_dragonspot = true,
 }
 
 local function patch_fn(color, orig_fn_name)
