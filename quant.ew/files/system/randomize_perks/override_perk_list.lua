3ee749c073911b113de711c6e075e5877ae6265f
IQuant <quant3234@gmail.com>
2025-01-18 20:01:59 +0300

    Fix #309

--- quant.ew/files/system/randomize_perks/override_perk_list.lua
+++ quant.ew/files/system/randomize_perks/override_perk_list.lua
@@ -2,6 +2,11 @@ local orig_perk_get_spawn_order = perk_get_spawn_order
 
 function perk_get_spawn_order(ignore_these_)
     local sx, sy = CrossCall("ew_per_peer_seed")
+    if sx == nil or sy == nil then
+        -- Fallback
+        sx = 0
+        sy = 0
+    end
     SetRandomSeed(1 + sx, 2 + sy)
     local orig_SetRandomSeed = SetRandomSeed
     function SetRandomSeed(x, y) end
