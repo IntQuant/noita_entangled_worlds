7fc5117b155252cb7a2d99894d9468208e2bb68a
bgkillas <walhelm@proton.me>
2025-01-19 15:18:48 -0500

    dont dupe gold for wand selling ig

--- quant.ew/files/system/wand_charm/append.lua
+++ quant.ew/files/system/wand_charm/append.lua
@@ -1,5 +1,13 @@
 local orig = material_area_checker_success
 
+local origl = EntityLoad
+function EntityLoad(path, x, y)
+    if path ~= "data/entities/items/pickup/goldnugget_200.xml"
+            or CrossCall("ew_do_i_own", GetUpdatedEntityID()) then
+        return origl(path, x, y)
+    end
+end
+
 function material_area_checker_success(pos_x, pos_y)
     if CrossCall("ew_do_i_own", GetUpdatedEntityID()) then
         CrossCall("ew_charm_sync", GetUpdatedEntityID())
