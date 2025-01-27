67542e3a36d1158b2a494fc20373324879fdb6ab
bgkillas <walhelm@proton.me>
2025-01-27 18:35:15 -0500

    fix kolmi for reals

--- quant.ew/files/system/kolmi/append/spawn_kolmi.lua
+++ quant.ew/files/system/kolmi/append/spawn_kolmi.lua
@@ -1,7 +1,16 @@
 local old = item_pickup
 function item_pickup(ent, run)
     if run == nil then
-        CrossCall("ew_spawn_kolmi", ent)
+        local gid
+        for _, v in ipairs(EntityGetComponent(ent, "VariableStorageComponent") or {}) do
+            if ComponentGetValue2(v, "name") == "ew_gid_lid" then
+                gid = v
+                break
+            end
+        end
+        if gid ~= nil and not ComponentGetValue2(gid, "value_bool") then
+            CrossCall("ew_spawn_kolmi", ComponentGetValue2(gid, "value_string"))
+        end
     end
     old(ent)
 end
