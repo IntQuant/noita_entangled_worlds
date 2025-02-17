8c8b6dc97c78765eb1def81891486b9948a4e77e
bgkillas <walhelm@proton.me>
2025-02-17 16:12:59 -0500

    maybe fix stevari dupe?

--- quant.ew/files/system/stevari/stevari.lua
+++ quant.ew/files/system/stevari/stevari.lua
@@ -1,7 +1,7 @@
 ModLuaFileAppend("data/scripts/biomes/temple_shared.lua", "mods/quant.ew/files/system/stevari/append.lua")
 
 local function request_flag_slow(x, y)
-    local flag = "ew_spawn_stevari" .. ":" .. math.floor(x / 512) .. ":" .. math.floor(y / 512)
+    local flag = "ew_spawn_stevari" .. ":" .. math.floor(x / 1024 + 0.5) .. ":" .. math.floor(y / 1024 + 0.5)
     net.send_flags("3" .. math.floor(x) .. " " .. math.floor(y) .. " " .. flag)
 end
 
