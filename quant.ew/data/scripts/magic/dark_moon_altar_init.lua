a03640eb6c9d4ea955b29fb8ebf431e0d614af44
bgkillas <walhelm@proton.me>
2025-02-19 11:43:08 -0500

    fix darksun

--- quant.ew/data/scripts/magic/dark_moon_altar_init.lua
+++ quant.ew/data/scripts/magic/dark_moon_altar_init.lua
@@ -1,5 +1,5 @@
 if HasFlagPersistent("darkmoon_is_darksun") then
     local entity_id = GetUpdatedEntityID()
     local x, y = EntityGetTransform(entity_id)
-    CrossCall("ew_moon_spawn", x, y)
+    CrossCall("ew_moon_spawn", x, y, true)
 end
