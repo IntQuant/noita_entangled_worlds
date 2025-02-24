507bd27134fbe4132d8037629eb31f829a0055f7
bgkillas <walhelm@proton.me>
2025-02-24 13:37:00 -0500

    documentation for once

--- ew_api_example/module.lua
+++ ew_api_example/module.lua
@@ -25,3 +25,26 @@ end
 return module
 -- Real implementation examples:
 -- https://github.com/Conga0/Apotheosis/pull/45/commits/ebc4544a3614736d6e394a953ba58fa229e6aa81x
+
+-- tags:
+-- player_unit, only on the local player, besides when a client fires a wand, just for the fire function
+-- ew_peer, on all players when not polied, unless notplayer(dead player in local health)
+-- ew_client, on all players besides your own, always
+-- ew_synced, syncs an entity that isn't synced otherwise, needed before next preupdate loop after entity is spawned
+-- ew_synced_var, syncs a variable storage component by name, needs to be on a synced entity
+-- ew_no_enemy_sync, dont sync an entity that would be synced otherwise, needed before next preupdate loop after entity is spawned
+-- ew_des, denotes if entity is synced by des, if unloaded and reloaded the entity will be culled on next preupdate loop
+-- ew_des_lua, on some lua components related to des
+-- ew_immortal, on some entitys you dont have auth over to fix damage numbers
+-- ew_notplayer, on notplayer
+-- ew_ghost_rnd, for something to sync perk ghosts rng
+-- ew_projectile_position_sync, syncs a projectile by des
+
+-- var components(by name not tag):
+-- ew_peer_id, on all peers, contains peerid in value_string
+-- ew_gid_lid, on all* synced entitys by des, value_string has gid(constant across reload),
+    -- value_int has lid(constant across clients but not reload),
+    -- value_bool is true iff you have authority over entity
+-- ew_frame_num, on mom or perk ghosts to sync position nicely
+-- ew_rng, on some entitys(dice) to sync rng
+-- ew_transmutation, on certain spells to sync rng
