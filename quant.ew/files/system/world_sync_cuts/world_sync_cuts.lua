f236ed635b5408f9ff20bb56c1e5e46ff4240451
bgkillas <walhelm@proton.me>
2025-02-27 23:24:23 -0500

    fix sadekivi being inconsistent

--- quant.ew/files/system/world_sync_cuts/world_sync_cuts.lua
+++ quant.ew/files/system/world_sync_cuts/world_sync_cuts.lua
@@ -1,7 +1,27 @@
+local rpc = net.new_rpc_namespace()
+
 util.add_cross_call("ew_cut_through_world", function(x, min_y, max_y, radius, _radius2)
     if ctx.is_host then
         net.proxy_send("cut_through_world", x .. " " .. min_y .. " " .. max_y .. " " .. radius)
     end
+end)
+
+util.prepend(
+    "data/scripts/items/beamstone_kick.lua",
+    'EntityLoad( "data/entities/misc/beam_from_sky.xml", pos_x, pos_y )',
+    'local util = dofile_once("mods/quant.ew/files/resource/util_min.lua")\n'
+        .. "if util.do_i_own(entity_id) then\n"
+        .. 'CrossCall("ew_beam", pos_x, pos_y)\n'
+        .. "done"
+)
+
+rpc.opts_everywhere()
+function rpc.beam(x, y)
+    EntityLoad("data/entities/misc/beam_from_sky.xml", x, y)
+end
+
+util.add_cross_call("ew_beam", function(x, y)
+    rpc.beam(x, y)
 end)
 
 ModLuaFileAppend(
@@ -9,6 +29,4 @@ ModLuaFileAppend(
     "mods/quant.ew/files/system/world_sync_cuts/patch_cut_through_world.lua"
 )
 
-local module = {}
-
-return module
+return {}
