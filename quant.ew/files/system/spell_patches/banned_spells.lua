9e80b862b4b5c2132fb874100c37c9c6b242e1b1
bgkillas <walhelm@proton.me>
2025-06-20 21:38:53 -0400

    fix spell ban

--- quant.ew/files/system/spell_patches/banned_spells.lua
+++ quant.ew/files/system/spell_patches/banned_spells.lua
@@ -1,27 +1,40 @@
 local function string_split(s, splitter)
     local words = {}
     if s == nil or splitter == nil or s == "" then
-        return {}
+        return words
     end
     for word in string.gmatch(s, "([^" .. splitter .. "]+)") do
         table.insert(words, word)
     end
     return words
 end
+local function contains(table, element)
+    for _, value in pairs(table) do
+        if value == element then
+            return true
+        end
+    end
+end
 local t = string_split(CrossCall("ew_banned_spells"), ",")
 local old = GetRandomActionWithType
 function GetRandomActionWithType(...)
-    local n = old
-    while table.contains(t, n) do
-        n = old
+    local n = old(...)
+    local a = { ... }
+    while contains(t, n) do
+        a[1] = a[1] + 1
+        a[2] = a[2] + 1
+        n = old(unpack(a))
     end
     return n
 end
-old = GetRandomAction
+local old2 = GetRandomAction
 function GetRandomAction(...)
-    local n = old
-    while table.contains(t, n) do
-        n = old
+    local n = old2(...)
+    local a = { ... }
+    while contains(t, n) do
+        a[1] = a[1] + 1
+        a[2] = a[2] + 1
+        n = old2(unpack(a))
     end
     return n
 end
