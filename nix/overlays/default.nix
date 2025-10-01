be993008c383795313f449270429151bf42f87d0
Jacob Birkett <jacob@birkett.dev>
2025-09-30 17:54:36 -0700

    nix+flake: packages+overlays: init noita-proxy

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -1,9 +1,14 @@
-{ self, lib, rust-overlay }: {
+{ self, lib, rust-overlay }:
+let rustPackageOverlay = import ./rust-package.nix;
+in {
   default = lib.composeManyExtensions [
     # This is to ensure that other overlays and invocations of `callPackage`
     # receive `rust-bin`, but without hard-coding a specific derivation.
     # This can be overridden by consumers.
     self.overlays.rust-overlay
+
+    # Packages provided by this flake:
+    self.overlays.noita-proxy
   ];
 
   # This flake exposes `overlays.rust-overlay` which is automatically applied
@@ -11,4 +16,12 @@
   # the package overlay, in the event it is not already present.
   rust-overlay = final: prev:
     if prev ? rust-bin then { } else rust-overlay.overlays.default final prev;
+
+  # The overlay definition uses `rust-bin` to construct a `rustPlatform`,
+  # and `rust-bin` is not provided by this particular overlay.
+  # Prefer using `overlays.default`, or composing with `rust-overlay` manually.
+  noita-proxy = rustPackageOverlay {
+    packageName = "noita-proxy";
+    sourceRoot = self;
+  };
 }
