6b716224f078b215166b2531549edb3fc3b3471f
bgkillas <walhelm@proton.me>
2025-11-14 19:21:32 -0500

    dont crash on bad from_id

--- noita_api_example/src/lib.rs
+++ noita_api_example/src/lib.rs
@@ -17,7 +17,11 @@ pub unsafe extern "C" fn luaopen_material_converter(lua: *mut lua_State) -> c_in
         LUA.lua_newuserdata(lua, 0);
         LUA.lua_createtable(lua, 0, 0);
         LUA.lua_setmetatable(lua, -2);
-        LUA.lua_setfield(lua, LUA_REGISTRYINDEX, c"luaclose_material_converter".as_ptr());
+        LUA.lua_setfield(
+            lua,
+            LUA_REGISTRYINDEX,
+            c"luaclose_material_converter".as_ptr(),
+        );
         add_lua_fn!(convert);
     }
     1
@@ -26,12 +30,15 @@ fn convert(lua: LuaState) -> eyre::Result<()> {
     let globals = Globals::default().game_global_mut();
     let from_id = lua.to_integer(1);
     let to_id = lua.to_integer(2);
-    let cosmetics = globals
+    let Some(cosmetics) = globals
         .m_cell_factory
         .cell_data
         .get(from_id as usize)
-        .unwrap()
-        .grid_cosmetic_particle_config;
+        .map(|data| data.grid_cosmetic_particle_config)
+    else {
+        noita_api::print!("{from_id} is not a real material >:(");
+        return Ok(());
+    };
     convert_material_everywhere(from_id as i32, to_id as i32)?;
     let data = globals
         .m_cell_factory
