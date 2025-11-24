040058fa6637620b5b99e24a06efdece0017d15f
bgkillas <walhelm@proton.me>
2025-11-24 11:33:39 -0500

    fix breaking on multiple vftables still

--- ewext/src/debug.rs
+++ ewext/src/debug.rs
@@ -1,17 +1,18 @@
 use crate::ExtState;
-use bimap::BiHashMap;
 use noita_api::addr_grabber::Globals;
+use noita_api::noita::types::*;
 use std::collections::HashMap;
+use windows::Win32::System::Memory::*;
 pub fn check_globals(_: &mut ExtState) -> eyre::Result<()> {
     let vftables = include_str!("../vftables.txt");
-    let mut name_map = BiHashMap::new();
+    let mut name_map = HashMap::new();
     let mut size_map = HashMap::new();
     for line in vftables.lines() {
         let mut split = line.split(' ');
         let Some(name) = split.next() else {
             continue;
         };
-        let mut name = name.to_string();
+        let name = name.to_string();
         let Some(addr) = split.next() else {
             continue;
         };
@@ -20,112 +21,66 @@ pub fn check_globals(_: &mut ExtState) -> eyre::Result<()> {
             continue;
         };
         let size = size.parse::<usize>().unwrap_or(0);
-        let mut i = 1;
-        while name_map.contains_right(&name) {
-            if i != 1 {
-                name.pop();
-            }
-            name = format!("{name}{i}");
-            i += 1;
-        }
         name_map.insert(addr, name);
         size_map.insert(addr, size);
     }
     let mut map = HashMap::new();
     for (addr, size) in size_map.into_iter() {
-        let name = name_map.remove_by_left(&addr).unwrap().1;
+        let name = name_map.remove(&addr).unwrap();
         map.insert(addr, (name, size));
     }
     let globals = Globals::default();
-    /*noita_api::print!(
-        "\n{}",
-        check_global(globals.entity_manager.cast(), &map, &mut Vec::new())
-    );
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.world_seed.cast(), &map, &mut Vec::new())
-    );
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.new_game_count.cast(), &map, &mut Vec::new())
-    );
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.global_stats.cast(), &map, &mut Vec::new())
-    );*/
-    Elem::from_addr(
-        unsafe {
-            globals
-                .game_global
-                .cast::<*const usize>()
-                .as_ref()
-                .copied()
-                .unwrap()
-        },
-        &map,
-        &mut Vec::new(),
-        "GameGlobal",
-        416,
-        0,
-    )
-    .print(0, 0);
-    /*noita_api::print!(
-        "\n{}",
-        check_global(globals.entity_tag_manager.cast(), &map, &mut Vec::new())
-    );
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.component_type_manager.cast(), &map, &mut Vec::new(),)
-    );
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.component_tag_manager.cast(), &map, &mut Vec::new(),)
-    );
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.translation_manager.cast(), &map, &mut Vec::new())
-    );
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.platform.cast(), &map, &mut Vec::new())
-    );
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.filenames.cast(), &map, &mut Vec::new())
-    );
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.inventory.cast(), &map, &mut Vec::new())
-    );
-    noita_api::print!("\n{}", check_global(globals.mods.cast(), &map, &mut Vec::new()));
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.max_component.cast(), &map, &mut Vec::new())
-    );
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.component_manager.cast(), &map, &mut Vec::new())
-    );
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.world_state.cast(), &map, &mut Vec::new())
-    );
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.world_state_component.cast(), &map, &mut Vec::new(),)
-    );
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.event_manager.cast(), &map, &mut Vec::new())
-    );
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.death_match.cast(), &map, &mut Vec::new())
-    );
-    noita_api::print!(
-        "\n{}",
-        check_global(globals.debug_settings.cast(), &map, &mut Vec::new())
-    );*/
+    let print = |addr: *const usize| {
+        check_global(addr, &map, &mut Vec::new(), 0, None).print(0, 0);
+    };
+    macro_rules! maybe_ptr_get {
+        (true, $name:tt) => {
+            unsafe { $name.cast::<*const usize>().as_ref().copied().unwrap() }
+        };
+        (false, $name:tt) => {
+            $name.cast()
+        };
+    }
+    macro_rules! print_unk {
+        ($addr:expr, $t:tt, $is_ref:tt) => {
+            Elem::from_addr(
+                maybe_ptr_get!($is_ref, $addr),
+                &map,
+                &mut Vec::new(),
+                stringify!($t),
+                size_of::<$t>(),
+                0,
+            )
+            .print(0, 0);
+        };
+    }
+    print(globals.entity_manager.cast());
+    print(globals.global_stats.cast());
+    print(globals.translation_manager.cast());
+    print(globals.platform.cast());
+    print(globals.death_match.cast());
+    print(globals.debug_settings.cast());
+    print_unk!(globals.component_type_manager, ComponentTypeManager, false);
+    print_unk!(globals.inventory, Inventory, false);
+    print_unk!(globals.game_global, GameGlobal, true);
+    print_unk!(globals.component_manager, ComponentSystemManager, false);
+    print_unk!(globals.world_state, Entity, true);
+    let ptr = globals.game_global().m_cell_factory as *const CellFactory;
+    print_unk!(ptr, CellFactory, false);
+    let ptr = globals.game_global().m_textures as *const Textures;
+    print_unk!(ptr, Textures, false);
+    let ptr = globals.game_global().m_game_world as *const GameWorld;
+    print_unk!(ptr, GameWorld, false);
+    let ptr = unsafe {
+        *(**globals.game_global)
+            .m_grid_world
+            .chunk_map
+            .chunk_array
+            .iter()
+            .find(|a| !a.is_null())
+            .unwrap()
+    } as *const Chunk;
+    print_unk!(ptr, Chunk, false);
     Ok(())
 }
 fn check_global(
@@ -133,6 +88,7 @@ fn check_global(
     map: &HashMap<usize, (String, usize)>,
     addrs: &mut Vec<*const usize>,
     entry: usize,
+    parent: Option<&str>,
 ) -> Elem {
     if let Some(n) = addrs.iter().position(|n| *n == reference) {
         return Elem::Recursive(addrs.len() - n, entry);
@@ -146,14 +102,18 @@ fn check_global(
             return Elem::Usize(entry);
         };
         if let Some((name, size)) = map.get(table) {
-            Elem::from_addr(reference, map, addrs, name, *size, entry)
+            if Some(name.as_ref()) == parent {
+                Elem::VFTable(entry)
+            } else {
+                Elem::from_addr(reference, map, addrs, name, *size, entry)
+            }
         } else if in_range(table)
             && let Some(inner) = (table as *const usize)
                 .cast::<*const usize>()
                 .as_ref()
                 .copied()
         {
-            Elem::Ref(Box::new(check_global(inner, map, addrs, entry)))
+            Elem::Ref(Box::new(check_global(inner, map, addrs, entry, None)))
         } else {
             Elem::Usize(entry)
         }
@@ -162,6 +122,7 @@ fn check_global(
 pub enum Elem {
     Ref(Box<Elem>),
     Struct(Struct, usize),
+    VFTable(usize),
     #[allow(unused)]
     Usize(usize),
     #[allow(unused)]
@@ -180,7 +141,7 @@ impl Elem {
         let mut i = 0;
         while i < size / 4 {
             let len = addrs.len();
-            let e = check_global(unsafe { reference.add(i) }, map, addrs, i);
+            let e = check_global(unsafe { reference.add(i) }, map, addrs, i, Some(name));
             if let Elem::Struct(_, size) = &e {
                 i += (*size).max(1);
             } else {
@@ -211,6 +172,9 @@ impl Elem {
             Elem::Recursive(_, _) => {
                 //noita_api::print!("{}[{e}]{}recursive<{k}>", "  ".repeat(n), "&".repeat(count))
             }
+            Elem::VFTable(e) => {
+                noita_api::print!("{}[{e}]{}VFTable", "  ".repeat(n), "&".repeat(count))
+            }
         }
     }
 }
@@ -235,7 +199,6 @@ impl Struct {
         }
     }
 }
-use windows::Win32::System::Memory::*;
 fn in_range(reference: *const usize) -> bool {
     if reference.is_null() {
         return false;
@@ -258,5 +221,6 @@ fn in_range(reference: *const usize) -> bool {
     if protect == PAGE_NOACCESS || protect == PAGE_GUARD {
         return false;
     }
+    //noita_api::print!("{:?} {:?}", reference, mbi);
     true
 }
