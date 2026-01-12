b65873999ef8bfc4988b4b30e429012234b2ed6f
Leo <76182388+merll002@users.noreply.github.com>
2026-01-12 20:47:35 +0000

    Update Noita launch script for better handling

--- scripts/start-lutris.sh
+++ scripts/start-lutris.sh
@@ -3,7 +3,7 @@
 if not pidof steam >/dev/null
 then
 	echo "Steam API is not available. If you want to play without port forwarding, Steam needs to be running."
-	read -rp "Do you want to start it now? [y/n] " choice
+	read -rp "Do you want to start it now? [y/N] " choice
 	choice="${choice,,}"
 	if [ "$choice" = "y" ] || [ "$choice" = "yes" ]
 	then 
@@ -13,12 +13,12 @@ then
 		echo "Running without Steam..."
 	fi
 fi
-ID=$(lutris --list-games 2>&1 | grep Noita | awk -F'|' '{ if (length($1) >= 2) print substr($1, length($1)-1, 1) }')
 
-echo "Noita Lutris instance found with ID: $ID"
 echo "Starting proxy..."
 
 chmod +x noita_proxy.x86_64
 
-LUTRIS_SKIP_INIT=1 ./noita_proxy.x86_64 --exe-path "/home/$USER/Games/gog/noita/drive_c/GOG Games/Noita/noita.exe" --launch-cmd "lutris lutris:rungameid/$ID"
+exepath=$(grep 'prefix: ' -m1 ~/.local/share/lutris/games/*noita* | tail -c +11)/$(cat ~/.local/share/lutris/games/*noita* | grep 'exe: ' -m1 | tail -c +8)
+name=$(grep 'game_slug: ' -m1 ~/.local/share/lutris/games/*noita* | tail -c +12)
+LUTRIS_SKIP_INIT=1 ./noita_proxy.x86_64 --exe-path "$exepath" --launch-cmd "lutris lutris:rungame/$name"
 
