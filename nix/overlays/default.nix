cc6b7df4ec16056866f14e5dd558424e530c5deb
Multirious <multirious@outlook.com>
2026-03-17 16:51:10 +0700

    Refactor nix

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -1,6 +1,12 @@
-{ self, lib, rust-overlay }:
-let rustPackageOverlay = import ./rust-package.nix;
-in {
+{
+  self,
+  lib,
+  rust-overlay,
+}:
+let
+  rustPackageOverlay = import ./rust-package.nix;
+in
+{
   default = lib.composeManyExtensions [
     # This is to ensure that other overlays and invocations of `callPackage`
     # receive `rust-bin`, but without hard-coding a specific derivation.
@@ -14,8 +20,8 @@ in {
   # This flake exposes `overlays.rust-overlay` which is automatically applied
   # by `overlays.default`. This overlay is intended to provide `rust-bin` for
   # the package overlay, in the event it is not already present.
-  rust-overlay = final: prev:
-    if prev ? rust-bin then { } else rust-overlay.overlays.default final prev;
+  rust-overlay =
+    final: prev: if prev ? rust-bin then { } else rust-overlay.overlays.default final prev;
 
   # The overlay definition uses `rust-bin` to construct a `rustPlatform`,
   # and `rust-bin` is not provided by this particular overlay.
