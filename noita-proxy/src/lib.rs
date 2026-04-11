fee192d3f94d01240dac71dedc5283eea84cae2b
Multirious <multirious@outlook.com>
2026-04-11 18:04:36 +0700

    Move Settings into its own module bookkeeping/settings with save/load impl

--- noita-proxy/src/lib.rs
+++ noita-proxy/src/lib.rs
@@ -8,17 +8,13 @@ pub use app::App;
 pub use cli::{Args, connect_cli, host_cli};
 pub use util::{lang, steam_helper};
 
-use app::AppSavedState;
 use audio_settings::AudioSettings;
 use bookkeeping::{mod_manager, releases};
 use game_map::ImageMap;
 use game_settings::{DefaultSettings, GameSettings};
 use lang::tr;
-use mod_manager::ModmanagerSettings;
 use player_cosmetics::player_path;
 use player_settings::{PlayerAppearance, PlayerColor, PlayerPicker};
-
-use serde::{Deserialize, Serialize};
 
 mod bookkeeping;
 mod lobby_code;
@@ -52,13 +48,4 @@ impl Drop for NetManStopOnDrop {
         self.0.continue_running.store(false, Ordering::Relaxed);
         self.1.take().unwrap().join().unwrap();
     }
-}
-
-#[derive(Debug, Serialize, Deserialize, Default)]
-#[serde(default)]
-pub struct Settings {
-    color: PlayerAppearance,
-    app: AppSavedState,
-    modmanager: ModmanagerSettings,
-    audio: AudioSettings,
 }
