aad7dc3705fa6f9a58a22f028e36cab9fe015459
Multirious <multirious@outlook.com>
2026-04-27 21:49:59 +0700

    Remove debug prints

--- quant.ew/files/system/heart_statue/script.lua
+++ quant.ew/files/system/heart_statue/script.lua
@@ -1,9 +1,7 @@
 function throw_item(xi, yi, xf, yf)
-    print_error("ew_heart_statue_throw")
     CrossCall("ew_heart_statue_throw", GetUpdatedEntityID())
 end
 
 function item_pickup()
-    print_error("ew_heart_statue_pickup")
     CrossCall("ew_heart_statue_pickup")
 end
