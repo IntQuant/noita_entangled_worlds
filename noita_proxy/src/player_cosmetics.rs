e5d5ff5b70207bccc225bb47a02335e676b7db2d
Multirious <multirious@outlook.com>
2026-05-08 21:05:56 +0700

    Update create_player_png

--- noita_proxy/src/player_cosmetics.rs
+++ noita_proxy/src/player_cosmetics.rs
@@ -1,6 +1,5 @@
 use std::{
-    ffi::OsString,
-    fs::{self, File, remove_file},
+    fs::{self, File},
     io::{BufRead, BufReader, Write},
     path::{Path, PathBuf},
     sync::MutexGuard,
@@ -11,10 +10,11 @@ use rustc_hash::FxHashMap;
 use shared::WorldPos;
 
 use crate::{
-    asset::{Asset, AssetManager},
+    asset::{Asset, AssetConfig, AssetManager},
     color::*,
     net::omni::OmniPeerId,
-    player_settings::{PlayerAppearance, PlayerColor},
+    player_settings::PlayerAppearance,
+    runtime_dir::RuntimeDir,
 };
 
 #[rustfmt::skip]
@@ -144,62 +144,6 @@ fn write_overlay_to_image(overlay: &RgbaImage, target: &mut RgbaImage) {
     }
 }
 
-pub fn arrows_path(path: PathBuf, is_host: bool) -> (PathBuf, PathBuf, PathBuf) {
-    let parent = path.parent().unwrap();
-    let p = parent.join("player_arrows");
-    let o = parent.join("player_ping");
-    (
-        if is_host {
-            p.join("arrow_host.png")
-        } else {
-            p.join("arrow.png")
-        },
-        o.join("arrow.png"),
-        parent.join("map/icon.png"),
-    )
-}
-
-pub fn cursor_path(path: PathBuf) -> PathBuf {
-    path.parent()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .join("resource/sprites/cursor.png")
-}
-
-pub fn replace_color(image: &mut RgbaImage, main: Rgba, alt: Rgba, arm: Rgba) {
-    let target_main = Rgba::from([155, 111, 154, 255]);
-    let target_alt = Rgba::from([127, 84, 118, 255]);
-    let target_arm = Rgba::from([89, 67, 84, 255]);
-    for pixel in image.pixels_mut() {
-        if compare_rgb(*pixel, target_main) {
-            set_rgb(pixel, main);
-        } else if compare_rgb(*pixel, target_alt) {
-            set_rgb(pixel, alt);
-        } else if compare_rgb(*pixel, target_arm) {
-            set_rgb(pixel, arm);
-        }
-    }
-}
-
-pub fn replace_color_opt(image: &mut RgbaImage, main: Rgba, alt: Rgba, arm: Rgba, inv: bool) {
-    let target_main = Rgba::from([155, 111, 154, 255]);
-    let target_alt = Rgba::from([127, 84, 118, 255]);
-    let target_arm = Rgba::from([89, 67, 84, 255]);
-    let target_border = Rgba::from([0, 0, 0, 255]);
-    for pixel in image.pixels_mut() {
-        if compare_rgb(*pixel, target_main) {
-            set_rgb(pixel, main);
-        } else if compare_rgb(*pixel, target_alt) {
-            set_rgb(pixel, alt);
-        } else if compare_rgb(*pixel, target_arm) {
-            set_rgb(pixel, arm);
-        } else if inv && compare_rgb(*pixel, target_border) {
-            set_rgb(pixel, invert(main));
-        }
-    }
-}
-
 pub fn make_player_preview(assets: &AssetManager, appearance: &PlayerAppearance) -> RgbaImage {
     let mut base = assets
         .get_parsed("player_preview_sprite")
@@ -237,209 +181,199 @@ pub fn make_player_preview(assets: &AssetManager, appearance: &PlayerAppearance)
     base
 }
 
-pub fn create_arm(arm: Rgba) -> RgbaImage {
-    let hand = Rgba::from([219, 192, 103, 255]);
-    let mut img = RgbaImage::new(5, 15);
-    for (i, pixel) in img.pixels_mut().enumerate() {
-        match i {
-            10 | 11 | 17 | 18 | 35 | 40 | 41 | 55 | 56 | 57 | 58 => *pixel = arm,
-            19 | 42 | 59 => *pixel = hand,
-            _ => {}
+fn make<'o>(
+    assets: &AssetManager,
+    colors: &[(&str, Rgba)],
+    sprites: &[(&str, &'o str)],
+) -> impl Iterator<Item = (RgbaImage, &'o str)> {
+    sprites.iter().map(move |(asset_name, output_suffix)| {
+        let mut base = assets.get_parsed(asset_name).as_image().to_rgba8();
+        for (color_name, color) in colors {
+            let mask = assets
+                .get_parsed(&format!("{asset_name}_{color_name}_mask"))
+                .as_image()
+                .to_rgba8();
+            write_color_rgb_with_mask_to_image(*color, &mask, &mut base);
         }
-    }
-    img
-}
-
-fn replace_colors(path: PathBuf, save: PathBuf, rgb: &PlayerColor) {
-    let mut img = image::open(path).unwrap().into_rgba8();
-    replace_color(
-        &mut img,
-        Rgba::from(to_u8(rgb.main)),
-        Rgba::from(to_u8(rgb.alt)),
-        Rgba::from(to_u8(rgb.arm)),
-    );
-    img.save(save).unwrap();
-}
-
-fn replace_colors_opt(path: PathBuf, save: PathBuf, rgb: &PlayerColor, inv: bool) {
-    let mut img = image::open(path).unwrap().into_rgba8();
-    replace_color_opt(
-        &mut img,
-        Rgba::from(to_u8(rgb.main)),
-        Rgba::from(to_u8(rgb.alt)),
-        Rgba::from(to_u8(rgb.arm)),
-        inv,
-    );
-    img.save(save).unwrap();
+        (base, *output_suffix)
+    })
 }
 
 #[allow(clippy::type_complexity)]
 pub fn create_player_png(
     peer: OmniPeerId,
-    mod_path: &Path,
-    player_path: &Path,
     assets: &AssetManager,
-    rgb: &PlayerAppearance,
+    runtime_dir: &RuntimeDir,
+    quantew_install: &Path,
+    appearance: &PlayerAppearance,
     is_host: bool,
     player_map: &mut MutexGuard<FxHashMap<OmniPeerId, (Option<WorldPos>, bool, bool, RgbaImage)>>,
 ) {
-    // let icon = get_player_skin(
-    //     image::open(player_path)
-    //         .unwrap_or(ImageRgba8(RgbaImage::new(20, 20)))
-    //         .crop(1, 1, 7, 16)
-    //         .into_rgba8(),
-    //     *rgb,
-    // );
-    let icon = make_player_preview(assets, rgb);
-    player_map.insert(peer, (None, false, false, icon.clone()));
-    let inv = rgb.invert_border;
     let id = peer.as_hex();
-    let cosmetics = rgb.cosmetics;
-    let rgb = rgb.color;
-    let tmp_path = player_path.parent().unwrap();
-    let (arrows_path, ping_path, map_icon) = arrows_path(tmp_path.into(), is_host);
-    let cursor_path = cursor_path(tmp_path.into());
-    let player_lukki = tmp_path.join("unmodified_lukki.png");
-    icon.save(tmp_path.join(format!("tmp/{id}_icon.png")))
+    let runtime_dir = runtime_dir.for_peer(peer);
+
+    let icon = make_player_preview(assets, appearance);
+    icon.save(runtime_dir.full_path("_icon.png")).unwrap();
+    player_map.insert(peer, (None, false, false, icon.clone()));
+
+    let colors = [
+        ("main", Rgba::from(to_u8(appearance.color.main))),
+        ("alt", Rgba::from(to_u8(appearance.color.alt))),
+        ("arm", Rgba::from(to_u8(appearance.color.arm))),
+    ];
+    let sprites = [
+        ("player_spritesheet", ".png"),
+        ("player_lukki_spritesheet", "_lukki.png"),
+        ("player_knee_sprite", "_knee.png"),
+        ("player_limb_a_sprite", "_limb_a.png"),
+        ("player_limb_b_sprite", "_limb_b.png"),
+        ("player_map_icon", "_map.png"),
+    ];
+    make(assets, &colors, &sprites)
+        .for_each(|(image, suffix)| image.save(runtime_dir.full_path(suffix)).unwrap());
+
+    let ragdoll_sprites = [
+        ("player_head_sprite", "_head.png"),
+        ("player_left_arm_sprite", "_left_arm.png"),
+        ("player_left_hand_sprite", "_left_hand.png"),
+        ("player_left_thigh_sprite", "_left_thigh.png"),
+        ("player_right_arm_sprite", "_right_arm.png"),
+        ("player_right_hand_sprite", "_right_hand.png"),
+        ("player_right_thigh_sprite", "_right_thigh.png"),
+        ("player_torso_sprite", "_torso.png"),
+    ];
+    make(assets, &colors, &ragdoll_sprites)
+        .for_each(|(image, suffix)| image.save(runtime_dir.full_path(suffix)).unwrap());
+
+    let ragdoll_list_path = runtime_dir.full_path("_ragdoll.txt");
+    let mut ragdoll_list_file = fs::File::options()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(ragdoll_list_path)
         .unwrap();
-    replace_colors(
-        player_path.into(),
-        tmp_path.join(format!("tmp/{id}.png")),
-        &rgb,
-    );
-    {
-        let mut img = image::open(player_path).unwrap().into_rgba8();
-        replace_color(
-            &mut img,
-            Rgba::from(to_u8(rgb.main)),
-            Rgba::from(to_u8(rgb.alt)),
-            Rgba::from(to_u8(rgb.arm)),
-        );
-        for px in img.pixels_mut() {
-            px.0[3] = px.0[3].min(64)
+    for (_, suffix) in ragdoll_sprites {
+        let path = runtime_dir.noita_path(suffix);
+        ragdoll_list_file
+            .write_all(path.as_os_str().as_encoded_bytes())
+            .unwrap();
+        ragdoll_list_file.write_all(b"\n").unwrap();
+    }
+
+    make(assets, &colors, &[("player_spritesheet", "_dc.png")]).for_each(|(mut image, suffix)| {
+        for px in image.pixels_mut() {
+            px.channels_mut()[3] = px.channels()[3].min(64)
         }
-        img.save(tmp_path.join(format!("tmp/{id}_dc.png"))).unwrap();
-    }
-    replace_colors(
-        player_lukki,
-        tmp_path.join(format!("tmp/{id}_lukki.png")),
-        &rgb,
-    );
-    replace_colors_opt(
-        arrows_path,
-        tmp_path.join(format!("tmp/{id}_arrow.png")),
-        &rgb,
-        inv,
-    );
-    replace_colors_opt(
-        ping_path,
-        tmp_path.join(format!("tmp/{id}_ping.png")),
-        &rgb,
-        inv,
-    );
-    replace_colors_opt(
-        cursor_path,
-        tmp_path.join(format!("tmp/{id}_cursor.png")),
-        &rgb,
-        inv,
-    );
-    replace_colors(
-        tmp_path.join("knee.png"),
-        tmp_path.join(format!("tmp/{id}_knee.png")),
-        &rgb,
-    );
-    replace_colors(
-        tmp_path.join("limb_a.png"),
-        tmp_path.join(format!("tmp/{id}_limb_a.png")),
-        &rgb,
-    );
-    replace_colors(
-        tmp_path.join("limb_b.png"),
-        tmp_path.join(format!("tmp/{id}_limb_b.png")),
-        &rgb,
-    );
-    replace_colors(map_icon, tmp_path.join(format!("tmp/{id}_map.png")), &rgb);
-    let ragdoll_path = tmp_path.join(format!("tmp/{id}_ragdoll.txt"));
-    if ragdoll_path.exists() {
-        remove_file(ragdoll_path.clone()).unwrap()
-    }
-    let mut ragdoll = File::create(ragdoll_path).unwrap();
-    let mut files = String::new();
-    for s in [
-        "head.png",
-        "left_hand.png",
-        "left_arm.png",
-        "left_thigh.png",
-        "right_hand.png",
-        "right_arm.png",
-        "right_thigh.png",
-        "torso.png",
-    ]
-    .iter()
-    .rev()
-    {
-        let f = tmp_path.join(s);
-        replace_colors(f, tmp_path.join(format!("tmp/{id}_ragdoll_{s}")), &rgb);
-        files = format!("{files}mods/quant.ew/files/system/player/tmp/{id}_ragdoll_{s}\n");
-    }
-    ragdoll.write_all(files.as_bytes()).unwrap();
-    let img = create_arm(Rgba::from(to_u8(rgb.forearm)));
-    let path = tmp_path.join(format!("tmp/{id}_arm.png"));
-    img.save(path).unwrap();
-    edit_nth_line(
-        tmp_path.join("unmodified_cape.xml").into(),
-        tmp_path.join(format!("tmp/{id}_cape.xml")).into_os_string(),
-        vec![16, 16],
-        vec![
-            format!("cloth_color=\"0xFF{}\"", rgb_to_hex(to_u8(rgb.cape))),
-            format!(
-                "cloth_color_edge=\"0xFF{}\"",
-                rgb_to_hex(to_u8(rgb.cape_edge))
+        image.save(runtime_dir.full_path(suffix)).unwrap();
+    });
+
+    make(
+        assets,
+        &[("forearm", Rgba::from(to_u8(appearance.color.forearm)))],
+        &[("player_arm_spritesheet", "_arm.png")],
+    )
+    .for_each(|(image, suffix)| image.save(runtime_dir.full_path(suffix)).unwrap());
+
+    let colors = [
+        ("main", Rgba::from(to_u8(appearance.color.main))),
+        ("alt", Rgba::from(to_u8(appearance.color.alt))),
+        ("arm", Rgba::from(to_u8(appearance.color.arm))),
+        (
+            "border",
+            // I suppose this is what the original code does
+            // even though the config make no sense.
+            if appearance.invert_border {
+                invert(Rgba::from(to_u8(appearance.color.main)))
+            } else {
+                Rgba::from([0, 0, 0, 255])
+            },
+        ),
+    ];
+    let sprites = [
+        (
+            if is_host {
+                "player_arrow_host_sprite"
+            } else {
+                "player_arrow_sprite"
+            },
+            "_arrow.png",
+        ),
+        ("player_ping_sprite", "_ping.png"),
+        ("player_cursor_sprite", "_cursor.png"),
+    ];
+    make(assets, &colors, &sprites)
+        .for_each(|(image, suffix)| image.save(runtime_dir.full_path(suffix)).unwrap());
+
+    edit_by_insert_line(
+        assets.get("player_cape_xml").unwrap().path(),
+        &runtime_dir.full_path("_cape.xml"),
+        &[
+            (
+                16,
+                format!(
+                    "cloth_color=\"0xFF{}\"",
+                    rgb_to_hex(to_u8(appearance.color.cape))
+                ),
+            ),
+            (
+                16,
+                format!(
+                    "cloth_color_edge=\"0xFF{}\"",
+                    rgb_to_hex(to_u8(appearance.color.cape_edge))
+                ),
             ),
         ],
     );
-    edit_nth_line(
-        tmp_path.join("unmodified.xml").into(),
-        tmp_path.join(format!("tmp/{id}_dc.xml")).into_os_string(),
-        vec![1],
-        vec![format!(
-            "filename=\"mods/quant.ew/files/system/player/tmp/{}_dc.png\"",
-            id
+    edit_by_insert_line(
+        assets.get("player_main_xml").unwrap().path(),
+        &runtime_dir.full_path("_dc.xml"),
+        &[(
+            1,
+            format!(
+                "filename=\"{}\"",
+                runtime_dir.noita_path("_dc.png").to_str().unwrap()
+            ),
         )],
     );
-    edit_nth_line(
-        tmp_path.join("unmodified.xml").into(),
-        tmp_path.join(format!("tmp/{id}.xml")).into_os_string(),
-        vec![1],
-        vec![format!(
-            "filename=\"mods/quant.ew/files/system/player/tmp/{}.png\"",
-            id
+    edit_by_insert_line(
+        assets.get("player_main_xml").unwrap().path(),
+        &runtime_dir.full_path(".xml"),
+        &[(
+            1,
+            format!(
+                "filename=\"{}\"",
+                &runtime_dir.noita_path(".png").to_str().unwrap(),
+            ),
         )],
     );
     edit_by_replacing(
-        tmp_path.join("unmodified_lukki.xml"),
-        tmp_path.join("tmp/".to_owned() + &id.clone() + "_lukki.xml"),
+        assets.get("player_lukki_xml").unwrap().path(),
+        &runtime_dir.full_path("_lukki.xml"),
         &[(
             "MARKER_LUKKI_PNG",
-            format!("mods/quant.ew/files/system/player/tmp/{id}_lukki.png"),
+            runtime_dir
+                .noita_path("_lukki.png")
+                .to_str()
+                .unwrap()
+                .to_string(),
         )],
     );
     edit_by_replacing(
-        tmp_path.join("unmodified_base.xml"),
-        tmp_path.join("tmp/".to_owned() + &id.clone() + "_base.xml"),
+        assets.get("player_base_xml").unwrap().path(),
+        &runtime_dir.full_path("_base.xml"),
         &[
             (
                 "MARKER_HAT2_ENABLED",
-                (if cosmetics.hat {
+                if appearance.cosmetics.hat {
                     "image_file=\"data/enemies_gfx/player_hat2.xml\""
                 } else {
                     ""
-                })
+                }
                 .into(),
             ),
             (
                 "MARKER_AMULET_ENABLED",
-                (if cosmetics.amulet {
+                (if appearance.cosmetics.amulet {
                     "image_file=\"data/enemies_gfx/player_amulet.xml\""
                 } else {
                     ""
@@ -448,7 +382,7 @@ pub fn create_player_png(
             ),
             (
                 "MARKER_AMULET_GEM_ENABLED",
-                (if cosmetics.amulet_gem {
+                (if appearance.cosmetics.amulet_gem {
                     "image_file=\"data/enemies_gfx/player_amulet_gem.xml\""
                 } else {
                     ""
@@ -457,67 +391,85 @@ pub fn create_player_png(
             ),
             (
                 "MARKER_MAIN_SPRITE",
-                format!("mods/quant.ew/files/system/player/tmp/{id}.xml"),
+                runtime_dir.noita_path(".xml").to_str().unwrap().to_string(),
             ),
             (
                 "MARKER_LUKKI_SPRITE",
-                format!("mods/quant.ew/files/system/player/tmp/{id}_lukki.xml"),
+                runtime_dir
+                    .noita_path("_lukki.xml")
+                    .to_str()
+                    .unwrap()
+                    .to_string(),
             ),
             (
                 "MARKER_ARM_SPRITE",
-                format!("mods/quant.ew/files/system/player/tmp/{id}_arm.xml"),
+                runtime_dir
+                    .noita_path("_arm.xml")
+                    .to_str()
+                    .unwrap()
+                    .to_string(),
             ),
             (
                 "MARKER_CAPE",
-                format!("mods/quant.ew/files/system/player/tmp/{id}_cape.xml"),
+                runtime_dir
+                    .noita_path("_cape.xml")
+                    .to_str()
+                    .unwrap()
+                    .to_string(),
             ),
             (
                 "RAGDOLL_MARKER",
-                format!("mods/quant.ew/files/system/player/tmp/{id}_ragdoll.txt"),
+                runtime_dir
+                    .noita_path("_ragdoll.txt")
+                    .to_str()
+                    .unwrap()
+                    .to_string(),
             ),
         ],
     );
-    edit_nth_line(
-        tmp_path.join("unmodified_arm.xml").into(),
-        tmp_path.join(format!("tmp/{id}_arm.xml")).into_os_string(),
-        vec![1],
-        vec![format!(
-            "filename=\"mods/quant.ew/files/system/player/tmp/{}_arm.png\"",
-            id
+    edit_by_insert_line(
+        assets.get("player_arm_xml").unwrap().path(),
+        &runtime_dir.full_path("_arm.xml"),
+        &[(
+            1,
+            format!(
+                "filename=\"{}\"",
+                &runtime_dir
+                    .noita_path("_arm.png")
+                    .to_str()
+                    .unwrap()
+                    .to_string()
+            ),
         )],
     );
     // mods.quant.ew.files.system.player.tmp.0000000000000001.png
     fs::copy(
-        tmp_path.join("player_uv.png"),
-        mod_path
-            .join("data/generated/sprite_uv_maps/")
+        assets.get("player_spritesheet_uv").unwrap().path(),
+        quantew_install
+            .join("data/generated/sprite_uv_maps")
             .join(format!("mods.quant.ew.files.system.player.tmp.{id}.png")),
     )
     .unwrap();
 }
 
-fn edit_nth_line(path: OsString, exit: OsString, v: Vec<usize>, newline: Vec<String>) {
-    let file = File::open(path).unwrap();
+fn edit_by_insert_line(input: &Path, output: &Path, to_insert: &[(usize, String)]) {
+    let file = File::open(input).unwrap();
     let reader = BufReader::new(file);
     let mut lines = reader.lines().map(|l| l.unwrap()).collect::<Vec<String>>();
-    for (i, line) in v.iter().zip(newline) {
-        lines.insert(*i, line);
+    for (i, line) in to_insert {
+        lines.insert(*i, line.clone());
     }
-    let mut file = File::create(exit).unwrap();
+    let mut file = File::create(output).unwrap();
     for line in lines {
         writeln!(file, "{line}").unwrap();
     }
 }
 
-fn edit_by_replacing(
-    path: impl AsRef<Path>,
-    out_path: impl AsRef<Path>,
-    replace_pair: &[(&'static str, String)],
-) {
+fn edit_by_replacing(input: &Path, output: &Path, replace_pair: &[(&'static str, String)]) {
     // Probably not a very good idea to unwrap here. Mod files should exist by this point, but...
-    let mut contents = fs::read_to_string(path).unwrap();
+    let mut contents = fs::read_to_string(input).unwrap();
     for pair in replace_pair {
         contents = contents.replace(pair.0, &pair.1);
     }
-    fs::write(out_path, contents).unwrap();
+    fs::write(output, contents).unwrap();
 }
